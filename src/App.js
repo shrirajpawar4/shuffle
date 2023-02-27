535a2fa3de257a898a878d31354e3b9cae4953da
shrirajpawar4 <shripawar0411@gmail.com>
2023-02-27 16:40:35 +0530

    GG :rocket:

--- src/App.js
+++ src/App.js
@@ -1,89 +1,172 @@
-import React, {useState} from "react";
+import { render } from "@testing-library/react";
+import React, {useEffect, useState} from "react";
+
+
+var arr  = ['Price Impact', 'Roi impact', 'Location', 'Amenities', 'Infrastructure'];
+
+var shuffle = async (arr) => {
+  let currentIndex = arr.length, randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+  } 
+
+  return arr;
+}
 
 
 function App() {
-  const arr  = ['Price Impact', 'Roi impact', 'Location', 'Amenities', 'Infrastructure'];
 
-  const [rodeo, setRodeo] = useState(null);
-  const [cowboy, setCowboy] = useState(null);
-  const [magician, setMagician] = useState(null);
 
-  const shuffle = (arr) => {
-    let currentIndex = arr.length, randomIndex;
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+  const [rodeo, setRodeo] = useState([]);
+  const [cowboy, setCowboy] = useState([]);
+  const [magician, setMagician] = useState([]);
 
-      [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
-    } 
 
-    return arr;
+  const handleRodeo = async () => {
+    var newarr = await shuffle(arr)
+    setRodeo(newarr)
+    console.log(newarr);
+  }
+
+  useEffect(() => {
+    handleRodeo();
+    console.log(rodeo);
+  })
+
+  const handleClickRodeo = async () => {
+    // if (rodeo.length < 1) {
+    //   await handleRodeo()
+    //   console.log(rodeo);
+    // }
+    console.log(rodeo);
+    render (
+      <div>
+      <table>
+        <thead> 
+          <tr>
+            <th>Attributes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rodeo.map((item, index) => (
+            <tr key={index}>
+              <td>{item}</td>
+            </tr>
+          ) )}
+        </tbody>
+      </table>
+      </div>
+    )
   }
 
 
-  const handleRodeo = () => {
-    if (rodeo === null) {
-      const newRodeo = shuffle(arr);
-      setRodeo(newRodeo);
-      console.log(newRodeo); 
-    } else {
-      console.log("already shuffled", rodeo);
-    }
+// cowboy function logic
+const handleCowboy = async () => {
+  var newarr = await shuffle(arr)
+  setCowboy(newarr)
+  console.log(newarr);
+}
 
+useEffect(() => {
+  handleCowboy();
+  console.log(cowboy);
+})
+
+  const handleClickCowboy = () => {
+    handleCowboy()
+    // if (cowboy === null) {
+    //   const newCowboy = shuffle(arr);
+    //   setCowboy(newCowboy);
+    //   console.log(newCowboy); 
+    // } else {
+    //   console.log("already shuffled", cowboy);
+    // }
+
+    render (
+      <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Attributes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cowboy.map((item, index) => (
+            <tr key={index}>
+              <td>{item}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    )
   }
 
+  //magician function logic
 
-  const handleCowboy = () => {
-    if (cowboy === null) {
-      const newCowboy = shuffle(arr);
-      setCowboy(newCowboy);
-      console.log(newCowboy); 
-    } else {
-      console.log("already shuffled", cowboy);
-    }
+  const handleMagician = async () => {
+    var newarr = await shuffle(arr)
+    setMagician(newarr)
+    console.log(newarr);
   }
+  
+  useEffect(() => {
+    handleMagician();
+    console.log(magician);
+  })
 
-  const handleMagician = () => {
-    if (magician === null) {
-      const newMagician = shuffle(arr);
-      setMagician(newMagician);
-      console.log(newMagician); 
-    } else {
-      console.log("already shuffled", magician);
-    }
+  const handleClickMagician = () => {
+    handleMagician()
+    
+    // if (magician === null) {
+    //   const newMagician = shuffle(arr);
+    //   setMagician(newMagician);
+    //   console.log(newMagician); 
+    // } else {
+    //   console.log("already shuffled", magician);
+    // }
+
+    render (
+      <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Attributes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {magician.map((item, index) => (
+            <tr key={index}>
+              <td>{item}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    )
   }
 
 
   return (
-    <>
     <div>
-    <input type="radio" id="rodeo" name="rodeo" value="rodeo" onClick={handleRodeo} />
-      <label htmlFor="rodeo">Rodeo Hat</label>
-
-      <input type="radio" id="cowboy" name="cowboy" value="cowboy" onClick={handleCowboy} />
-      <label htmlFor="cowboy">Cowboy Hat</label>
-
-      <input type="radio" id="magician" name="magician" value="magician" onClick={handleMagician} />
-      <label htmlFor="magician">Magician Hat</label> 
-    </div>
-
     <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Attributes</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rodeo.map((item, index) => (
-          <tr key={index}>
-            <td>{item}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <input type="radio" id="rodeo" name="hat" value="rodeo" onClick={handleClickRodeo} />
+    <label htmlFor="magician">Rodeo Hat</label> 
+
+    <input type="radio" id="cowboy" name="hat"  value="cowboy" onClick={handleClickCowboy} />
+    <label htmlFor="magician">Cowboy Hat</label> 
+    
+
+    <input type="radio" id="magician" name="hat" value="magician" onClick={handleClickMagician} />
+    <label htmlFor="magician">Magician Hat</label> 
     </div>
-    </>
+    <div>
+      
+    </div>
+    </div>
   );
 }
 
